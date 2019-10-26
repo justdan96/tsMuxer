721d5c12d9591cd48d376b28f19ace39b8fc6cf1
Daniel Kamil Kozar <dkk089@gmail.com>
2019-10-26 19:24:21 +0200

    Use std::thread in libmediation's TerminatableThread
    TerminatableThread is now implemented by using std::thread. The terminate()
    function provided by the base class was actually not used anywhere, and was
    removed along with the base class itself.
    As a bonus, the completely unused files common_win32.{cpp,h} are also removed.

---
+++ libmediation/system/terminatablethread.cpp
@@ -0,0 +1,13 @@
+#include "terminatablethread.h"
+
+#include <cassert>
+
+TerminatableThread::~TerminatableThread() {
+  join();
+}
+
+void TerminatableThread::run(TerminatableThread *const t) {
+  assert(t->m_thread.get_id() == std::thread::id() &&
+         "TerminatableThread::run() called on an already running thread");
+  t->m_thread = std::thread([t]() { t->thread_main(); });
+}
--- libmediation/system/terminatablethread.h
+++ libmediation/system/terminatablethread.h
@@ -1,40 +1,31 @@
+#ifndef TERMINATABLE_THREAD_H
+#define TERMINATABLE_THREAD_H
 
-#ifndef _TERMINATABLE_THREAD_H
-#define _TERMINATABLE_THREAD_H
+#include <thread>
 
-class  Terminatable
-{
-public:
-	virtual ~Terminatable();
-
-	virtual void terminate() = 0;
-};
-
-class  TerminatableThread: public Terminatable
+class  TerminatableThread
 {
 public:
 	//! After creating the thread object, it's necessary to call run(thread) until the thread's first usage.
-	TerminatableThread();
+	TerminatableThread() {}
 	//! The destructor waits until the thread ends its work and destroys the object.
 	virtual ~TerminatableThread(); 
 
 	//! Launch the thread. Should be called immediately after creating the thread object.
 	static void run( TerminatableThread* const );
 
-	void join();
-    
-
+	void join() {
+		if (m_thread.joinable()) {
+			m_thread.join();
+		}
+	}
 
 protected:
 	//! Main thread function. Should be implemented in derived classes.
 	virtual void thread_main() = 0;
 
 private:
-	TerminatableThread( const TerminatableThread& );
-	TerminatableThread& operator=( const TerminatableThread& );
-
-	friend void threadProc( TerminatableThread* const ptr );
-	void* const m_impl;
+	std::thread m_thread;
 };
 
-#endif //_TERMINATABLE_THREAD_H
+#endif //TERMINATABLE_THREAD_H
