c48cce889bd274ba1bf379158dcd0164953f8329
Dan Bryant <daniel.bryant@linux.com>
2019-12-14 12:23:39 +0000

    improve output on upload script

--- bintray_nightly_upload.sh
+++ bintray_nightly_upload.sh
@@ -22,15 +22,19 @@ else
     echo "version $version_date has been created!"
     
     # upload the ZIP files to the version we just created on bintray
+    echo "uploading files..."
     curl -T ./bin/mac.zip -u$BINTRAY_USER:$BINTRAY_API_KEY -H "X-Bintray-Package:$PCK_NAME" -H "X-Bintray-Version:$version_date"  -H "X-Bintray-Publish:1"  -H "X-Bintray-Override:1" https://api.bintray.com/content/$BINTRAY_USER/$BINTRAY_REPO/mac-nightly-$version_date.zip
     curl -T ./bin/w32.zip -u$BINTRAY_USER:$BINTRAY_API_KEY -H "X-Bintray-Package:$PCK_NAME" -H "X-Bintray-Version:$version_date"  -H "X-Bintray-Publish:1"  -H "X-Bintray-Override:1" https://api.bintray.com/content/$BINTRAY_USER/$BINTRAY_REPO/w32-nightly-$version_date.zip
     curl -T ./bin/w64.zip -u$BINTRAY_USER:$BINTRAY_API_KEY -H "X-Bintray-Package:$PCK_NAME" -H "X-Bintray-Version:$version_date"  -H "X-Bintray-Publish:1"  -H "X-Bintray-Override:1" https://api.bintray.com/content/$BINTRAY_USER/$BINTRAY_REPO/w64-nightly-$version_date.zip
     curl -T ./bin/lnx.zip -u$BINTRAY_USER:$BINTRAY_API_KEY -H "X-Bintray-Package:$PCK_NAME" -H "X-Bintray-Version:$version_date"  -H "X-Bintray-Publish:1"  -H "X-Bintray-Override:1" https://api.bintray.com/content/$BINTRAY_USER/$BINTRAY_REPO/lnx-nightly-$version_date.zip
+    echo "files uploaded!"
     
     # update the latest commit on bintray
+    echo "updating commit record on bintray..."
     echo $local_commit > commit.txt
     curl -T commit.txt -u$BINTRAY_USER:$BINTRAY_API_KEY -H "X-Bintray-Package:commit" -H "X-Bintray-Version:latest"  -H "X-Bintray-Publish:1"  -H "X-Bintray-Override:1" https://api.bintray.com/content/$BINTRAY_USER/$BINTRAY_REPO/commit.txt
     rm -f commit.txt
+    echo "commit record updated!"
   else
     echo "error creating version $version_date !"
     exit 2
