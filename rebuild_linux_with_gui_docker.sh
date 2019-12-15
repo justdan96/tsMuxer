ac8f6cfcb71f1a329e84486c7afea7d0074d46ed
Dan Bryant <daniel.bryant@linux.com>
2019-12-15 20:28:14 +0000

    add AppImage packaging for tsMuxerGUI on Linux

--- rebuild_linux_with_gui_docker.sh
+++ rebuild_linux_with_gui_docker.sh
@@ -9,6 +9,36 @@ cd ..
 rm -rf build
 mkdir ./bin/lnx
 mv ./bin/tsMuxeR ./bin/lnx/tsMuxeR
-mv ./bin/tsMuxerGUI ./bin/lnx/tsMuxerGUI
-zip -jr ./bin/lnx.zip ./bin/lnx
-ls ./bin/lnx/tsMuxeR && ls ./bin/lnx/tsMuxerGUI && ls ./bin/lnx.zip
+
+# create AppImage of the GUI
+
+mkdir -p ./bin/lnx/AppDir/usr/share/applications
+mkdir -p ./bin/lnx/AppDir/usr/share/icons/hicolor/32x32/apps
+mkdir -p ./bin/lnx/AppDir/usr/bin
+
+mv ./bin/tsMuxerGUI ./bin/lnx/AppDir/usr/bin/tsMuxerGUI
+
+cp ./tsMuxerGUI/images/icon.png ./bin/lnx/AppDir/usr/share/icons/hicolor/32x32/apps/tsMuxerGUI.png
+
+cat << EOF > ./bin/lnx/AppDir/usr/share/applications/tsmuxergui.desktop
+[Desktop Entry]
+Name=tsMuxerGUI
+Comment=tsMuxerGUI
+Exec=tsMuxerGUI
+Icon=tsMuxerGUI
+Terminal=false
+Type=Application
+Categories=AudioVideo;
+StartupNotify=true
+EOF
+
+cd ./bin/lnx
+
+/opt/linuxdeploy/usr/bin/linuxdeploy --appdir AppDir --plugin qt --output appimage
+
+cd ../..
+
+rm -rf ./bin/lnx/AppDir
+
+zip -r ./bin/lnx.zip ./bin/lnx
+ls ./bin/lnx/tsMuxeR && ls ./bin/lnx/tsMuxerGUI-*-x86_64.AppImage && ls ./bin/lnx.zip
