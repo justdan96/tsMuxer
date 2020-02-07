eebfebc3f329b6a351449f99b36327c87a37d8f3
abakum <koka.abakum@gmail.com>
2020-02-07 16:07:37 +0300

    Create msys2.sh

---
+++ msys2.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+if [ "$MSYSTEM" == "MSYS" ] ; then
+ pacman -Syu
+ pacman -Sy --needed base-devel \
+ flex \
+ zlib-devel
+else
+ if [ ! -d build ] ; then
+  pacman -Sy --needed $MINGW_PACKAGE_PREFIX-toolchain \
+  $MINGW_PACKAGE_PREFIX-cmake \
+  $MINGW_PACKAGE_PREFIX-freetype \
+  $MINGW_PACKAGE_PREFIX-zlib \
+  $MINGW_PACKAGE_PREFIX-ninja
+  echo If you intend to build the tsMuxerGUI enter Y
+  pacman -S --needed $MINGW_PACKAGE_PREFIX-qt5-static
+  if [ -d $MINGW_PREFIX/qt5-static ] ; then
+   echo 'load(win32/windows_vulkan_sdk)' > $MINGW_PREFIX/qt5-static/share/qt5/mkspecs/common/windows-vulkan.conf
+   echo 'QMAKE_LIBS_VULKAN       =' >>     $MINGW_PREFIX/qt5-static/share/qt5/mkspecs/common/windows-vulkan.conf
+  fi
+  mkdir build
+ fi
+ cd build
+ cmake .. -G Ninja
+ ninja && cp -u tsMuxer/tsmuxer.exe ../bin/
+ if [ -d $MINGW_PREFIX/qt5-static ] ; then
+  $MINGW_PREFIX/qt5-static/bin/qmake ../tsMuxerGUI
+  make && cp -u debug/tsMuxerGUI.exe ../bin/
+ fi
+ cd ..
+fi
