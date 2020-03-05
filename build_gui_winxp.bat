24bb790a084143ad5dd1318d5c8828b66c1605ae
Daniel Kamil Kozar <dkk089@gmail.com>
2020-03-05 20:55:46 +0100

    switch to the build from my repository on github

--- build_gui_winxp.bat
+++ build_gui_winxp.bat
@@ -1,6 +1,6 @@
 
 mkdir C:\Qt\
-powershell -Command "Invoke-WebRequest https://files.catbox.moe/y5f5be.7z -O qt-xp-static.7z"
+powershell -Command "Invoke-WebRequest https://github.com/xavery/qt563xp/releases/download/1.0/Qt-5.6.3-Static-XP.7z -O qt-xp-static.7z"
 7z x -oC:\Qt\ qt-xp-static.7z
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
