59f22eb4813a7c0328f70eec18b947b41984232e
Daniel Kamil Kozar <dkk089@gmail.com>
2020-04-28 23:49:48 +0200

    Provide a script and workflow for building the project natively on MacOS (#145)

---
+++ build_macos_native.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+
+export MACOSX_DEPLOYMENT_TARGET=10.10
+
+brew install pkg-config
+brew install freetype
+brew install zlib
+
+builddir=$PWD
+
+mkdir build
+
+pushd build
+cmake -DCMAKE_BUILD_TYPE=Release -DTSMUXER_GUI=TRUE ..
+make -j$(sysctl -n hw.logicalcpu)
+
+pushd tsMuxerGUI
+pushd tsMuxerGUI.app/Contents
+defaults write "$PWD/Info.plist" NSPrincipalClass -string NSApplication
+defaults write "$PWD/Info.plist" NSHighResolutionCapable -string True
+plutil -convert xml1 Info.plist
+popd
+macdeployqt tsMuxerGUI.app
+popd
+
+mkdir bin
+pushd bin
+mv ../tsMuxer/tsmuxer tsMuxeR
+mv ../tsMuxerGUI/tsMuxerGUI.app .
+BZIP2=-9 tar cjf mac.tar.bz2 tsMuxeR tsMuxerGUI.app
+popd
+popd
