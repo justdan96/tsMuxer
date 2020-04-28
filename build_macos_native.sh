fe7d7f9e3846abc0d08e5491e4410d3bd181c2c5
Daniel Kamil Kozar <dkk089@gmail.com>
2020-04-28 23:22:31 +0200

    make plist changes before running macdeployqt, use tar for packaging

--- build_macos_native.sh
+++ build_macos_native.sh
@@ -11,23 +11,23 @@ brew install zlib
 
 builddir=$PWD
 
-mkdir -p bin/mac
-mkdir -p build
+mkdir build
 
 pushd build
 cmake -DCMAKE_BUILD_TYPE=Release -DTSMUXER_GUI=TRUE ..
-make
+make -j$(sysctl -n hw.logicalcpu)
 
-pushd tsMuxerGUI
-macdeployqt tsMuxerGUI.app
 pushd tsMuxerGUI.app/Contents
 defaults write "$PWD/Info.plist" NSPrincipalClass -string NSApplication
 defaults write "$PWD/Info.plist" NSHighResolutionCapable -string True
 plutil -convert xml1 Info.plist
 popd
+
+pushd tsMuxerGUI
+macdeployqt tsMuxerGUI.app
 popd
 
-mv tsMuxer/tsmuxer "${builddir}/bin/mac/tsMuxeR"
-mv tsMuxerGUI/tsMuxerGUI.app "${builddir}/bin/mac"
+mv tsMuxer/tsmuxer "$PWD/tsMuxeR"
+mv tsMuxerGUI/tsMuxerGUI.app "$PWD"
+BZIP2=-9 tar cjf mac.tar.bz2 tsMuxeR tsMuxerGUI.app
 popd
-
