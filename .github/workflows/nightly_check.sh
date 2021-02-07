e1270286b5c72be18ae5d1c60230e96b0feb3636
Daniel Kamil Kozar <dkk089@gmail.com>
2021-02-07 14:01:35 +0100

    remove bintray scripts, move functionality to nightly.yml

--- .github/workflows/nightly_check.sh
+++ .github/workflows/nightly_check.sh
@@ -5,7 +5,7 @@ if [[ $? -eq 0 ]]; then
   # build not needed : latest 'nightly-*' tag points at HEAD
   exit 1
 else
-  # build needed : output the desired tag name and exit 0
-  echo "nightly-$(date +%Y-%m-%d-%H-%M-%S)"
+  # build needed : output the build timestamp and exit 0
+  echo "$(date +%Y-%m-%d-%H-%M-%S)"
   exit 0
 fi
