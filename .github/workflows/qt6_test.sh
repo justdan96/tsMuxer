d1f6ce9d34aa2b1f8b07eaca0e4f5f2dc5e945ca
Daniel Kamil Kozar <dkk089@gmail.com>
2021-11-11 18:33:59 +0100

    add support for Qt6 in cmake

--- .github/workflows/qt6_test.sh
+++ .github/workflows/qt6_test.sh
@@ -1,6 +1,15 @@
 cd tsMuxerGUI
+
 rm -fR build
 mkdir build
 cd build
 qmake ..
 make -j$(nproc --all)
+
+cd ..
+
+rm -fR build
+mkdir build
+cd build
+cmake -DTSMUXER_GUI=TRUE -DQT_VERSION=6 -GNinja ..
+ninja tsMuxerGUI
