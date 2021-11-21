5d399134c2c37462b91b628b72d306aa4cedb8b6
Daniel Kamil Kozar <dkk089@gmail.com>
2021-11-21 01:46:35 +0100

    add libpng

--- build_macos_native.sh
+++ build_macos_native.sh
@@ -10,8 +10,30 @@ if ! num_cores=$(sysctl -n hw.logicalcpu); then
 fi
 
 topdir=$PWD
-ft_installdir="$topdir/ft_install"
-mkdir -p "$ft_installdir"
+deps_installdir="$topdir/.deps_install"
+mkdir -p "${deps_installdir}"
+
+download()
+{
+  curl -O -L "$1"
+}
+
+build_libpng()
+{
+  local png_ver='1.6.37'
+  local pkg_name='libpng-${png_ver}'
+  local pkg_fname="${pkg_name}.tar.xz"
+
+  download "https://download.sourceforge.net/libpng/${pkg_fname}"
+  tar xf "$pkg_fname"
+  pushd "$pkg_name"
+
+  ./configure --prefix="${deps_installdir}" --enable-shared=no
+  make -j${num_cores}
+  make install
+
+  popd
+}
 
 build_freetype()
 {
@@ -21,17 +43,20 @@ build_freetype()
 
   mkdir freetype_install
 
-  curl -O -L "https://download.savannah.gnu.org/releases/freetype/${pkg_fname}"
+  download "https://download.savannah.gnu.org/releases/freetype/${pkg_fname}"
   tar xf "${pkg_fname}"
   pushd "${pkg_name}"
 
   mkdir build
   pushd build
 
-  # the default flags cause the build to depend on bzip2 and zlib only. this is
-  # in line with the package that's build by homebrew.
-  cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=FALSE
-    "-DCMAKE_INSTALL_PREFIX=${ft_installdir}" ..
+  # this aims to replicate the brew package's functionality, which uses
+  # system-provided bzip2 and zlib, and requires libpng which we provide
+  # ourselves as a static library.
+  cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=FALSE \
+    -DFT_WITH_PNG=TRUE -DFT_WITH_ZLIB=TRUE -DFT_WITH_BZIP2=TRUE \
+    -DCMAKE_INSTALL_PREFIX="${deps_installdir}" \
+    -DCMAKE_PREFIX_PATH="${deps_installdir}" ..
   make -j${num_cores}
   make install
 
@@ -45,7 +70,8 @@ build_tsmuxer()
 
   pushd build
   cmake -DCMAKE_BUILD_TYPE=Release -DTSMUXER_GUI=TRUE \
-    "-DCMAKE_PREFIX_PATH=${ft_installdir}" ..
+    "-DCMAKE_PREFIX_PATH=${ft_installdir}" \
+    '-DFREETYPE_EXTRA_LIBRARIES=z;bz2' ..
   make -j${num_cores}
 
   pushd tsMuxerGUI
@@ -67,5 +93,10 @@ build_tsmuxer()
   popd
 }
 
+mkdir .deps
+pushd .deps
+build_libpng
 build_freetype
+popd
+
 build_tsmuxer
