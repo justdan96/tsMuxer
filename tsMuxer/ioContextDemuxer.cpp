justdan96/tsMuxer

commit e9cfbce4ef9189f1ebbef10b9758a56ca3bbb352

Initialise class member variables

tsMuxer/ioContextDemuxer.cpp
@@ -30,7 +30,8 @@ float av_int2flt(int32_t v)
     return ldexp((float)((v & 0x7FFFFF) + (1 << 23)) * (v >> 31 | 1), (v >> 23 & 0xFF) - 150);
 }
 
-IOContextDemuxer::IOContextDemuxer(const BufferedReaderManager& readManager) : m_readManager(readManager)
+IOContextDemuxer::IOContextDemuxer(const BufferedReaderManager& readManager)
+    : m_readManager(readManager), m_lastReadRez(0), tracks()
 {
     m_lastProcessedBytes = 0;
     m_bufferedReader = (const_cast<BufferedReaderManager&>(m_readManager)).getReader("");
@@ -52,7 +53,7 @@ int IOContextDemuxer::get_byte()
         uint8_t* data = m_bufferedReader->readBlock(m_readerID, readedBytes, readRez);  // blocked read mode
         if (readedBytes > 0 && readRez == 0)
             m_bufferedReader->notify(m_readerID, readedBytes);
-        m_lastReadRez = readRez;
+        // m_lastReadRez = readRez;
         m_curPos = data + 188;
         m_bufEnd = m_curPos + readedBytes;
         if (m_curPos == m_bufEnd)

tsMuxer/combinedH264Demuxer.cpp
@@ -15,7 +15,7 @@ static const int MAX_TMP_BUFFER_SIZE = 128;
 
 // ------------------------ CombinedH264Reader -------------------------------
 
-CombinedH264Reader::CombinedH264Reader()
+CombinedH264Reader::CombinedH264Reader() : m_demuxedPID(0), m_state(ReadState_Primary)
 {
     m_firstDemuxCall = true;
     m_mvcSPS = -1;

tsMuxer/dvbSubStreamReader.h
@@ -8,7 +8,16 @@
 class DVBSubStreamReader : public SimplePacketizerReader
 {
    public:
-    DVBSubStreamReader() : SimplePacketizerReader(), m_big_offsets(0), m_frameDuration(0), m_firstFrame(true) {}
+    DVBSubStreamReader()
+        : SimplePacketizerReader(),
+          m_big_offsets(0),
+          m_frameDuration(0),
+          m_firstFrame(true),
+          m_end_display_time(0),
+          m_offset_size(0),
+          m_start_display_time(0)
+    {
+    }
     int getTSDescriptor(uint8_t* dstBuff, bool blurayMode, bool hdmvDescriptors) override;
 
    protected:

tsMuxer/hevc.cpp
@@ -391,8 +391,7 @@ int HevcSpsUnit::vui_parameters()
     bool aspect_ratio_info_present_flag = m_reader.getBit();
     if (aspect_ratio_info_present_flag)
     {
-        int aspect_ratio_idc = m_reader.getBits(8);
-        if (aspect_ratio_idc == EXTENDED_SAR)
+        if (m_reader.getBits(8) == EXTENDED_SAR)  // // aspect_ratio_idc
         {
             int sar_width = m_reader.getBits(16);
             int sar_height = m_reader.getBits(16);

tsMuxer/mpeg2StreamReader.h
@@ -55,7 +55,6 @@ class MPEG2StreamReader : public MPEGStreamReader
     int decodePicture(uint8_t* buff);
     int processExtStartCode(uint8_t* buff);
     int processSeqStartCode(uint8_t* buff);
-    uint8_t* m_nextFrameAddr;
 };
 
 #endif

tsMuxer/tsPacket.cpp
@@ -116,7 +116,7 @@ bool TS_program_association_section::deserialize(uint8_t* buffer, int buf_size)
         transport_stream_id = bitReader.getBits(16);
         bitReader.skipBits(2);  // reserved
         bitReader.getBits(5);   // int version_number
-        bitReader.getBits(1);   // int current_next_indicator
+        bitReader.getBit();   // int current_next_indicator
 
         bitReader.getBits(8);  // int section_number
         bitReader.getBits(8);  // int last_section_number
@@ -277,7 +277,7 @@ bool TS_program_map_section::deserialize(uint8_t* buffer, int buf_size)
         program_number = bitReader.getBits(16);
         bitReader.skipBits(2);                         // reserved
         bitReader.getBits(5);                          // int version_number
-        int nextIndicator = bitReader.getBits(1);      // int current_next_indicator
+        int nextIndicator = bitReader.getBit();      // int current_next_indicator
         int sectionNumber = bitReader.getBits(8);      // int section_number
         int lastSectionNumber = bitReader.getBits(8);  // int last_section_number
         bitReader.skipBits(3);                         // reserved

tsMuxer/vc1StreamReader.cpp
@@ -360,9 +360,6 @@ int VC1StreamReader::getNextBFrames(uint8_t* buffer, int64_t& bTiming)
     for (uint8_t* nal = VC1Unit::findNextMarker(buffer, m_bufEnd); nal < m_bufEnd - 4;
          nal = VC1Unit::findNextMarker(nal + 4, m_bufEnd))
     {
-        if (nal[3] != (uint8_t)VC1Code::VC1_CODE_FIELD && nal[3] != (uint8_t)VC1Code::VC1_USER_CODE_FIELD)
-            m_nextFrameAddr = nal;
-
         if (nal[3] == (uint8_t)VC1Code::VC1_CODE_FRAME || nal[3] == (uint8_t)VC1Code::VC1_USER_CODE_FRAME)
         {
             VC1Frame frame;
@@ -395,12 +392,10 @@ int VC1StreamReader::getNextBFrames(uint8_t* buffer, int64_t& bTiming)
     }
     if (m_eof)
     {
-        m_nextFrameAddr = m_bufEnd;
         return bFrameCnt;
     }
     else
     {
-        m_nextFrameAddr = 0;
         return -1;
     }
 }

commit 7d1faf8a695375c41283f203c59542aa5a8f2113

Fix bug introduced by e9cfbce4

tsMuxer/ioContextDemuxer.cpp
@@ -53,7 +53,7 @@ int IOContextDemuxer::get_byte()
         uint8_t* data = m_bufferedReader->readBlock(m_readerID, readedBytes, readRez);  // blocked read mode
         if (readedBytes > 0 && readRez == 0)
             m_bufferedReader->notify(m_readerID, readedBytes);
-        // m_lastReadRez = readRez;
+        m_lastReadRez = readRez;
         m_curPos = data + 188;
         m_bufEnd = m_curPos + readedBytes;
         if (m_curPos == m_bufEnd)