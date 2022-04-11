1b772e6f7483ab6a731b2436ce685f5a3897b0fb
Daniel Kamil Kozar <dkk089@gmail.com>
2022-04-11 23:52:41 +0200

    add cmake to path

--- scripts/build_macos_arm64.sh
+++ scripts/build_macos_arm64.sh
@@ -4,7 +4,7 @@ export MACOSX_DEPLOYMENT_TARGET=11.0
 qtver=6
 
 export QT_HOME="$HOME/Qt/6.2.4/macos"
-export PATH="/opt/homebrew/bin:$QT_HOME/bin:$PATH"
+export PATH="/opt/homebrew/bin:$QT_HOME/bin:$HOME/Qt/Tools/CMake/CMake.app/Contents/bin:$PATH"
 EXTRA_CMAKE_ARGS=("-DCMAKE_PREFIX_PATH=$QT_HOME/lib/cmake")
 
 source scripts/macos_common_build.sh
