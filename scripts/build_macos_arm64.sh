f38b72d78da105443265d42dda7bf673b6491321
Daniel Kamil Kozar <dkk089@gmail.com>
2022-04-12 00:06:26 +0200

    ugh

--- scripts/build_macos_arm64.sh
+++ scripts/build_macos_arm64.sh
@@ -5,6 +5,6 @@ qtver=6
 
 export QT_HOME="$HOME/Qt/6.2.4/macos"
 export PATH="/opt/homebrew/bin:$QT_HOME/bin:$HOME/Qt/Tools/CMake/CMake.app/Contents/bin:$PATH"
-EXTRA_CMAKE_ARGS=("-DCMAKE_PREFIX_PATH=$QT_HOME/lib/cmake;$(brew --prefix)")
+EXTRA_CMAKE_ARGS=("-DCMAKE_PREFIX_PATH=$QT_HOME/lib/cmake;$(brew --prefix)" '-DCMAKE_OSX_ARCHITECTURES=arm64')
 
 source scripts/macos_common_build.sh
