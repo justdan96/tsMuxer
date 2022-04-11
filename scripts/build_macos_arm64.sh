d429ae6bba5fb3bbe3e65876e8b3882ea979c103
Daniel Kamil Kozar <dkk089@gmail.com>
2022-04-11 23:50:35 +0200

    this is not going well

--- scripts/build_macos_arm64.sh
+++ scripts/build_macos_arm64.sh
@@ -4,7 +4,7 @@ export MACOSX_DEPLOYMENT_TARGET=11.0
 qtver=6
 
 export QT_HOME="$HOME/Qt/6.2.4/macos"
-export PATH="/opt/homebrew/bin:$QT_HOME/bin"
+export PATH="/opt/homebrew/bin:$QT_HOME/bin:$PATH"
 EXTRA_CMAKE_ARGS=("-DCMAKE_PREFIX_PATH=$QT_HOME/lib/cmake")
 
 source scripts/macos_common_build.sh
