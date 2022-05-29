d1f72f9335274d18c28ebcfc6671f145b8a42aaf
jcdr428 <56721609+jcdr428@users.noreply.github.com>
2022-05-29 19:07:39 +0100

    Update rebuild_mxe32_with_gui_docker.sh
    Uploads unzipped files only in /bin floder.

--- scripts/rebuild_mxe32_with_gui_docker.sh
+++ scripts/rebuild_mxe32_with_gui_docker.sh
@@ -1,21 +1,17 @@
 export PATH=/usr/lib/mxe/usr/bin:$PATH
 rm -rf build
 mkdir build
+mkdir ./bin/w32
 cd build
 export CCACHE_DISABLE=1
 export MXE_USE_CCACHE=
+
 i686-w64-mingw32.static-cmake ../
 make
-cp tsMuxer/tsmuxer.exe ../bin/tsMuxeR.exe
+mv tsMuxer/tsmuxer.exe ../bin/w32/tsMuxeR.exe
 
 i686-w64-mingw32.static-qmake-qt5 ../tsMuxerGUI
 make
-cp ./tsMuxerGUI.exe ../bin/
+mv ./tsMuxerGUI.exe ../bin/w32/tsMuxerGUI.exe
 cd ..
 rm -rf build
-
-mkdir ./bin/w32
-mv ./bin/tsMuxeR.exe ./bin/w32/tsMuxeR.exe
-mv ./bin/tsMuxerGUI.exe ./bin/w32/tsMuxerGUI.exe
-zip -jr ./bin/w32.zip ./bin/w32
-ls ./bin/w32/tsMuxeR.exe && ls ./bin/w32/tsMuxerGUI.exe && ls ./bin/w32.zip
