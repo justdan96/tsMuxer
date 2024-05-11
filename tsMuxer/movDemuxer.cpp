cb0455259bb9182fba466a4905b840fed3a72646
Jean Christophe DE RYCK <jessiedeer@hotmail.com>
2024-05-11 17:39:56 +0100

    Fix issue #877  Regression on commit 5f43ab2

--- tsMuxer/movDemuxer.cpp
+++ tsMuxer/movDemuxer.cpp
@@ -726,8 +726,7 @@ int MovDemuxer::simpleDemuxBlock(DemuxedData& demuxedData, const PIDSet& accepte
         }
     }
     const int64_t startPos = m_processedBytes;
-    while (m_processedBytes - startPos < m_fileBlockSize && m_curChunk < chunks.size() &&
-           m_curChunk < m_mdat_data.size())
+    while (m_processedBytes - startPos < m_fileBlockSize && m_curChunk < chunks.size())
     {
         const int64_t offset = chunks[m_curChunk].first;
         int64_t next;
